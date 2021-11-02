fe4ed42acb8e76a19341c960a56bc420f57d2b4e
Daniel Guinter <guinterdaniel@gmail.com>
2021-11-02 15:10:11 -0300

    ssh

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
 
         <div className="row px-3">
           <div className="col-sm-6">
-            <h6 className= "text-center text-secundary">Taxa de sucesso (%)</h6>
+            <h5 className= "text-center text-secundary">Taxa de sucesso (%)</h5>
             <BarChart />
           </div>
           <div className="col-sm-6">
